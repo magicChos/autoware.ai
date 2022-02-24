8b30f4d54902d6418fb17c8893bf822fe8aa3a40
magicChos <hscoder@163.com>
2022-02-25 00:22:50 +0800

    read naive_motion_predict

--- src/autoware/core_perception/naive_motion_predict/nodes/naive_motion_predict/naive_motion_predict_node.cpp
+++ src/autoware/core_perception/naive_motion_predict/nodes/naive_motion_predict/naive_motion_predict_node.cpp
@@ -16,10 +16,10 @@
 
 #include "naive_motion_predict.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "naive_motion_predict");
-  NaiveMotionPredict node;
-  ros::spin();
-  return 0;
+    ros::init(argc, argv, "naive_motion_predict");
+    NaiveMotionPredict node;
+    ros::spin();
+    return 0;
 }
